38d6f74cb87ed75d7a280b044f83a6b507165d72
v02qabs <takesue090@gmail.com>
2021-12-05 00:08:37 +0900

    wirecube-x64-linux

---
+++ glut/gldraw/wirecube/main.c
@@ -0,0 +1,25 @@
+#include <GL/glut.h>
+
+void display(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(1.0f,1.0f,1.0f);
+  glutWireCube(0.5);
+  glFlush();
+}
+
+void init(void)
+{
+  glClearColor(0.0, 0.0, 1.0, 1.0);
+}
+
+int main(int argc, char *argv[])
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGBA);
+  glutCreateWindow(argv[0]);
+  glutDisplayFunc(display);
+  init();
+  glutMainLoop();
+  return 0;
+}
